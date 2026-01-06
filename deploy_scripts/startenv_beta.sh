5b652eb5a39d32a6d8840dccc9c37d180c27addc
Cyrus <570406098@qq.com>
2026-01-06 11:50:11 +0800

    fix(deploy): 修正conda环境和依赖安装脚本路径问题
    更新conda版本检查和python版本显示
    使用绝对路径安装requirements.txt

--- deploy_scripts/startenv_beta.sh
+++ deploy_scripts/startenv_beta.sh
@@ -4,11 +4,14 @@ echo "conda环境和依赖安装开始"
 export PATH="/home/q/www/miniconda3/bin:$PATH"
 conda init bash
 source ~/.bashrc
-conda activate py311
 
+
+conda -version
+conda activate py311
+python --version
 echo "conda切换结束"
 
-pip install -r requirements.txt -i http://devpi.corp.qunar.com/qunar/dev/+simple/ --trusted-host devpi.corp.qunar.com
+pip install -r /home/q/www/pf_king_crimson/webapps/ROOT/requirements.txt -i http://devpi.corp.qunar.com/qunar/dev/+simple/ --trusted-host devpi.corp.qunar.com
 
 export AUTO_ENV_NAME="beta"
 
